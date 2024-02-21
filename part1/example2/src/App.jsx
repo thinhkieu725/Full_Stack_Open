77c6c3926ab58b553f75ec224e99859183f369cc
thinhkieu725 <thinhkieu725@gmail.com>
2024-02-22 01:58:35 +0200

    Add example 2

---
+++ part1/example2/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+const Display = ({ counter }) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <Display counter={counter}/>
+
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
+    </div>
+  )
+}
+
+export default App
