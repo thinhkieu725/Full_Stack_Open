ab377ad319faf9ba1c69e2bae6dab1d192fc162b
dxthki <thinh.kieu@tuni.fi>
2024-03-13 00:41:44 +0200

    uibuild

--- part4/notes/backend/controllers/notes.js
+++ part4/notes/backend/controllers/notes.js
@@ -1,25 +1,21 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', (request, response) => {
-  Note.find({}).then(notes => {
-    response.json(notes)
-  })
+notesRouter.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes);
 })
 
-notesRouter.get('/:id', (request, response, next) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) {
-        response.json(note)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+notesRouter.get('/:id', async (request, response, next) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response, next) => {
   const body = request.body
 
   const note = new Note({
@@ -27,19 +23,15 @@ notesRouter.post('/', (request, response, next) => {
     important: body.important || false,
   })
 
-  note.save()
-    .then(savedNote => {
-      response.json(savedNote)
-    })
-    .catch(error => next(error))
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+
+  await Note.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+
 })
 
 notesRouter.put('/:id', (request, response, next) => {
@@ -54,7 +46,6 @@ notesRouter.put('/:id', (request, response, next) => {
     .then(updatedNote => {
       response.json(updatedNote)
     })
-    .catch(error => next(error))
 })
 
 module.exports = notesRouter
---
+++ part4/notes/backend/mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack1:${password}@cluster0.ztsnlyc.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+  const urlForTesting =
+  `mongodb+srv://fullstack1:${password}@cluster0.ztsnlyc.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(urlForTesting).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
+  })
+
+  const Note = mongoose.model('Note', noteSchema)
+
+  
+  const note = new Note({
+    content: 'HTML is easy',
+    important: true,
+  })
+
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+})
